97db14bb2b6bef3f1cf6fd78574e49c44759a965
Hill <hill@kimo.com>
2016-01-24 11:07:09 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+[![Latest Stable Version](https://poser.pugx.org/pmvc-app/error/v/stable)](https://packagist.org/packages/pmvc-app/error) 
+[![Latest Unstable Version](https://poser.pugx.org/pmvc-app/error/v/unstable)](https://packagist.org/packages/pmvc-app/error) 
+[![Build Status](https://travis-ci.org/pmvc-app/error.svg?branch=master)](https://travis-ci.org/pmvc-app/error)
+[![License](https://poser.pugx.org/pmvc-app/error/license)](https://packagist.org/packages/pmvc-app/error)
+[![Total Downloads](https://poser.pugx.org/pmvc-app/error/downloads)](https://packagist.org/packages/pmvc-app/error) 
+
+# PMVC Error Handle Share App 
+===============
+
+## Install with Composer
+### 1. Download composer
+   * mkdir test_folder
+   * curl -sS https://getcomposer.org/installer | php
+
+### 2. Install Use composer.json or use command-line directly
+#### 2.1 Install Use composer.json
+   * vim composer.json
+```
+{
+    "require": {
+        "pmvc-app/error": "dev-master"
+    }
+}
+```
+   * php composer.phar install
+
+#### 2.2 Or use composer command-line
+   * php composer.phar require pmvc-app/error
+
+
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+namespace PMVC\App\Error;
+
+$b = new \PMVC\MappingBuilder();
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\ErrorAction';
+${_INIT_CONFIG}[_INIT_BUILDER] = $b;
+
+$b->addAction('index', array(
+    _FUNCTION=>array(${_INIT_CONFIG}[_CLASS],'index')
+));
+
+$b->addForward('error', array(
+    _PATH=>'error'
+    ,_TYPE=>'view'
+));
+
+class ErrorAction extends \PMVC\Action
+{
+    static function index($m, $f){
+        $defineds = \PMVC\fromJson(file_get_contents(__DIR__.'/errors.json'));
+        $errors = array();
+        if (isset($f['errors'])) {
+            $errorIds = $f['errors'];
+            foreach ($errorIds as $id) {
+                if (!empty($defineds->{$id})) {
+                    $errors[] = new Error($defineds->{$id});
+                }
+            }
+        }
+        $go = $m['error'];
+        $go->set('errors',$errors);
+        return $go;
+    }
+}
+
+class Error
+{
+    public $message;
+    public $field; 
+    public $url;
+
+    function __construct($data)
+    {
+        $this->message = $data->message;
+        $this->field = $data->field;
+        $this->url = $data->url;
+    }
+}
+
+
+
+
+
